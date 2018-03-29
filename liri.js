025517ca2edd04bf5042a2a67b9dab0a70a51b11
Christian Lopes <christianlopes@dhcp-v094-073.mobile.uci.edu>
2018-03-28 18:27:39 -0700

    fully functional

--- liri.js
+++ liri.js
@@ -4,9 +4,9 @@ var Spotify = require('node-spotify-api')
 var request = require('request');
 var Twitter = require('twitter')
 
-var variable = process.argv[3]
-var input = process.argv[2]
-
+// argv is "array-like"
+var command = process.argv[2];
+var variable = process.argv.splice(3).join(" ");
 
 // Twitter Key
 var client = new Twitter({
@@ -23,7 +23,7 @@ var spotify = new Spotify({
 });
 
 
-switch (input) {
+switch (command) {
   //Twitter call
   case 'my-tweets':
     client.get('statuses/user_timeline', { screen_name: variable }, function (error, tweets, response) {
@@ -44,36 +44,42 @@ switch (input) {
 
   //OMDB Call
   case 'movie-this':
+
+  if (variable === "") {
+    variable = "Mr. Nobody";
+  }
     // OMDB API Request
     request('http://omdbapi.com/?apikey=trilogy&t=' + variable, function (error, response, body) {
       if (error) {
         console.log(error)
       }
       else {
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      console.log('Title:', JSON.parse(body).Title);
-      console.log('Year:', JSON.parse(body).Year);
-      console.log('IMDB Rating:', JSON.parse(body).imdbRating);
-      console.log('Rotten Tomatoes Rating:', JSON.parse(body).Ratings[1]);
-      console.log('Country:', JSON.parse(body).Country);
-      console.log('Language:', JSON.parse(body).Language);
-      console.log('Plot:', JSON.parse(body).Plot);
-      console.log('Actors:', JSON.parse(body).Actors);
-      console.log("\n-------------\n");
-
-      debugger
-
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('Title:', JSON.parse(body).Title);
+        console.log('Year:', JSON.parse(body).Year);
+        console.log('IMDB Rating:', JSON.parse(body).imdbRating);
+        console.log('Rotten Tomatoes Rating:', JSON.parse(body).Ratings[1]);
+        console.log('Country:', JSON.parse(body).Country);
+        console.log('Language:', JSON.parse(body).Language);
+        console.log('Plot:', JSON.parse(body).Plot);
+        console.log('Actors:', JSON.parse(body).Actors);
+        console.log("\n-------------\n");
       }
     });
     break
 
   //Spotify Call
   case 'spotify-this-song':
+
+    if (variable === "") {
+      variable = "The Sign";
+    }
+
     spotify.search({ type: 'track', query: variable }, function (err, data) {
+      //console.log('data', data);
       if (err) {
         return console.log('Error occurred: ' + err);
       }
-
       else {
         for (var i = 0; i < 20; i++) {
           console.log('Artist: ' + data.tracks.items[i].artists[0].name);
@@ -83,6 +89,8 @@ switch (input) {
           console.log("\n-------------\n");
         }
       }
+
     });
+
     break;
 }
